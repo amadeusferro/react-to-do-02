33ad82b586f70112e3f6f4bde8912e8653139190
Amadeus Ferro <108826489+amadeusferro@users.noreply.github.com>
2023-10-08 18:52:55 -0300

    Add files via upload

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useState } from 'react';
+
+function TodoForm({addTodo}) {
+    const [value, setValue] = useState("");
+    const [category, setCategory] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!value || !category) return;
+        
+        addTodo(value, category)
+
+        setCategory("");
+        setValue("");
+    }
+
+
+  return <div className='todo-form'>
+    <h2>Criar tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+            <input value={value} type="text" placeholder='Digite o título da tarefa' onChange={(e) => setValue(e.target.value)} />
+            <select value={category} onChange={(e) => setCategory(e.target.value)}>
+                <option value="">Selecione uma categoria</option>
+                <option value="Trabalho">Trabalho</option>
+                <option value="Lazer">Lazer</option>
+                <option value="Estudo">Estudo</option>
+            </select>
+            <button type='submit'>Criar tarefa</button>
+        </form>
+  </div>;
+}
+
+export default TodoForm
